04aacc05f31dd23c97f06a5e7d0baeea8bd4ff79
Gaston <gastonsilvestrebrc@gmail.com>
2022-07-25 14:51:48 -0300

    test finalizado del ejercicio2

--- tests/test_ejercicio2.py
+++ tests/test_ejercicio2.py
@@ -6,14 +6,29 @@
 from src.ejercicio2 import estadistica
 
 """
-Describir aquí que es lo que se esta probando.
-Tengan en cuenta que el archivo tiene que llamarse igual
-que el archivo a probar agregando antes `test_`
+En este test se estará probando los posibles caminos para la
+función de estadística. El objetivo es ejercitar todos los caminos
+del algoritmo.
 """
 
 
-def test_nombrefuncion():
+def test_estadistica_1():
     """
-    Una breve descripción del caso de prueba aplicado a la función
+    En esta funcion ejercitamos las primeras tres estructuras
+    condicionales if, finalizando el programa sin pasar por el while.
     """
-    pass
+    secuencia = (5,6)
+    resultado = estadistica(secuencia)
+    assert isinstance(resultado, tuple), "El resultado no es tuple."
+    assert resultado == (6,5,5.5), "Resultado no esperado." #(mayor,menor,promedio)
+    
+def test_estadistica_2():
+    """
+    En esta función ejercitamos la primera estructura condicional repetitiva,
+    donde dentro tiene dos estructuras condicionales que también se
+    ejercitarán, finalizando el programa.
+    """
+    secuencia = (1,2,3,4)
+    resultado = estadistica(secuencia)
+    assert isinstance(resultado, tuple), "El resultado no es tuple."
+    assert resultado == (4,1,2.5), "Resultado no esperado."
